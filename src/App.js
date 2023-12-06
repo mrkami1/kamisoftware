c85c2027fe0c2b0a6027ac34d5327155fbf10192
mrkami1 <mashhood.anwar@gmail.com>
2023-12-05 19:33:09 -0500

    switched to hash router

--- src/App.js
+++ src/App.js
@@ -4,19 +4,19 @@ import FynatrackPrivacy from './routes/fynatrack_privacy';
 import Home from './routes/home'
 import ContactUs from './routes/contactus';
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <HashRouter>
             <Routes>
                 <Route exact path='/' element={<Home />}/>
                 <Route exact path='/contact-us' element={<ContactUs />}/>
                 <Route exact path='/fynatrack' element={<Fynatrack />}/>
                 <Route exact path='/fynatrack-privacy' element={<FynatrackPrivacy />}/>
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     </div>
   );
 }
